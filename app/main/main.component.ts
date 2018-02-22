356db5b582b443fc7c20abb1adec62069698a305
Megan Franco <francomegan@gmail.com>
2018-02-21 21:11:11 -0500

    begin to experiment with model driven forms

---
+++ app/main/main.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, FormGroup, FormControl, FormBuilder, Validators  } from '@angular/forms';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+  public badgeForm: FormGroup;
+
+  constructor(private _fb: FormBuilder) { }
+
+  ngOnInit() {
+    //what we want, { first name, last name, }, { city and state, neightborhood, job},
+    // { favorite color/animal }
+    this.badgeForm = new FormGroup({
+      name: new FormControl( '', [<any>Validators.required, <any>Validators.minLength(5)] ),
+      address: new FormGroup ({
+        city: new FormControl ('', <any>Validators.required),
+        state: new FormControl ('', <any>Validators.required),
+        neighborhood: new FormControl ('', <any>Validators.required),
+        zip: new FormControl ('', <any>Validators.required)
+      }),
+      favorites: new FormGroup({
+         animal: new FormControl ('', <any>Validators.required),
+         color:  new FormControl ('', <any>Validators.required)
+      })
+    })
+  }
+
+}
